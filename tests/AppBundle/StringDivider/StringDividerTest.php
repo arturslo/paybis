b07b1f608d9db5e3e116b2f3e60783d0a09ee4c4
Artūrs Ločmelis <arturs.locmelis.89@gmail.com>
2018-11-06 10:16:48 +0200

    add stringDivider result test cases

--- tests/AppBundle/StringDivider/StringDividerTest.php
+++ tests/AppBundle/StringDivider/StringDividerTest.php
@@ -37,4 +37,54 @@ class StringDividerTest extends TestCase
         $stringDivider = new StringDivider();
         $stringDivider->divideIntoSubstrings($dividerRequest);
     }
+
+    public function test_empty_string_return_empty_array()
+    {
+        $dividerRequest = new DividerRequest('', 1);
+        $stringDivider = new StringDivider();
+        $substringCollection = $stringDivider->divideIntoSubstrings($dividerRequest);
+
+        $this->assertEquals([], $substringCollection);
+    }
+
+    public function test_one_element_string_with_minimal_substring_1()
+    {
+        $dividerRequest = new DividerRequest('a', 1);
+        $stringDivider = new StringDivider();
+        $substringCollection = $stringDivider->divideIntoSubstrings($dividerRequest);
+
+        $this->assertEquals([['a']], $substringCollection);
+    }
+
+    public function test_2_element_string_min_divider_1()
+    {
+        $dividerRequest = new DividerRequest('ab', 1);
+        $stringDivider = new StringDivider();
+        $substringCollection = $stringDivider->divideIntoSubstrings($dividerRequest);
+
+        $this->assertEquals([['a', 'b'], ['ab']], $substringCollection);
+    }
+
+    public function test_min_substring_larger_than_string_element_count_returns_empty_array()
+    {
+        $dividerRequest = new DividerRequest('ab', 6);
+        $stringDivider = new StringDivider();
+        $substringCollection = $stringDivider->divideIntoSubstrings($dividerRequest);
+
+        $this->assertEquals([], $substringCollection);
+    }
+
+    public function test_divide_into_substrings_diffrent_chars()
+    {
+        $dividerRequest = new DividerRequest('xyz', 1);
+        $stringDivider = new StringDivider();
+        $substringCollection = $stringDivider->divideIntoSubstrings($dividerRequest);
+
+        $this->assertEquals([
+            ['x', 'y', 'z'],
+            ['xy', 'z'],
+            ['x', 'yz'],
+            ['xyz']
+        ], $substringCollection);
+    }
 }

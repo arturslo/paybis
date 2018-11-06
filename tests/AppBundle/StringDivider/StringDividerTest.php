6e89126cbc301794b91692e183bbe1ed6ce0a18e
Artūrs Ločmelis <arturs.locmelis.89@gmail.com>
2018-11-06 10:04:05 +0200

    add minimal substring length tests

--- tests/AppBundle/StringDivider/StringDividerTest.php
+++ tests/AppBundle/StringDivider/StringDividerTest.php
@@ -8,12 +8,9 @@ use PHPUnit\Framework\TestCase;
 class StringDividerTest extends TestCase
 {
 
-    public function testDivideIntoSubstrings()
+    public function test_divide_into_substrings()
     {
-        $inputString = 'abc';
-        $minimalSubstringLength = 1;
-
-        $dividerRequest = new DividerRequest($inputString, $minimalSubstringLength);
+        $dividerRequest = new DividerRequest('abc', 1);
         $stringDivider = new StringDivider();
         $substringCollection = $stringDivider->divideIntoSubstrings($dividerRequest);
 
@@ -25,4 +22,19 @@ class StringDividerTest extends TestCase
         ], $substringCollection);
     }
 
+    public function test_minimal_substring_length_equal_to_zero_throws_exception()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $dividerRequest = new DividerRequest('abc', 0);
+        $stringDivider = new StringDivider();
+        $stringDivider->divideIntoSubstrings($dividerRequest);
+    }
+
+    public function test_minimal_substring_length_smaller_than_zero_throws_exception()
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $dividerRequest = new DividerRequest('abc', -1);
+        $stringDivider = new StringDivider();
+        $stringDivider->divideIntoSubstrings($dividerRequest);
+    }
 }
